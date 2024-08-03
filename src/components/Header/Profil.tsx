8d53a81af1381b82fc29bd19da3d5d6a70c17665
Sofiane Rahmani <sofiane.rahmani12@gmail.com>
2024-08-03 20:42:52 +0200

    docs: :speech_balloon: change mail pro

--- src/components/Header/Profil.tsx
+++ src/components/Header/Profil.tsx
@@ -40,8 +40,8 @@ export default function Profil() {
 
           <div className="flex items-center justify-start gap-2">
             <MailIcon />
-            <a href="mailto:sofiane.rahmani12@gmail.com?subject=Contact professionnel : [NOM DE VOTRE ENTREPRISE]" className="text-[#0073aa] hover:underline">
-              sofiane.rahmani12@gmail.com
+            <a href="mailto:contact@sofiane-rahmani.com?subject=Contact professionnel : [NOM DE VOTRE ENTREPRISE]" className="text-[#0073aa] hover:underline">
+              contact@sofiane-rahmani.com
             </a>
           </div>
 
